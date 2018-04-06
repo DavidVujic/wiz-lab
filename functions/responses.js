8ff8c5d77ff411cb6d1c4037045c0c79fffb4306
davidvujic <david@vujic.se>
2018-04-06 12:11:53 +0200

    remove code

--- functions/responses.js
+++ functions/responses.js
@@ -1,18 +1,5 @@
 function handleRequest (context, req) {
-  context.log('JavaScript HTTP trigger function processed a request.')
-
   context.bindings.outputBlob = req.body
-
-  if (req.body) {
-    context.res = {
-      body: req.body
-    }
-  } else {
-    context.res = {
-      status: 400,
-      body: ''
-    }
-  }
 
   context.done()
 }
